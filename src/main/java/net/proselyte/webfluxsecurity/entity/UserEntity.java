a0acfc85df0b505386b4ce71ce52c1aff76944b9
test80git <test80git@yandex.ru>
2026-01-28 19:31:17 +0300

    SpringBoot version 3.2.0 UserEntity добавлены аннотации api.http Help.MD

--- src/main/java/net/proselyte/webfluxsecurity/entity/UserEntity.java
+++ src/main/java/net/proselyte/webfluxsecurity/entity/UserEntity.java
@@ -1,7 +1,10 @@
 package net.proselyte.webfluxsecurity.entity;
 
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
@@ -20,7 +23,12 @@ public class UserEntity {
     private String firstName;
     private String lastName;
     private boolean enabled;
+    @CreatedDate
+    @Column("created_at")
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column("updated_at")
     private LocalDateTime updatedAt;
 
     @ToString.Include(name = "password")
